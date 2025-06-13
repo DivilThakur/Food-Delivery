d64f48cdcfbdd2bb80b70af98d44f330b722bd32
Divil Thakur <Divilthkr3@gmail.com>
2025-06-13 14:40:33 +0530

    loading state

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -14,11 +14,21 @@ import Orders from "./components/Orders/Orders";
 import SignInModal from "./components/Auth/SignInModal";
 import { ToastContainer } from "react-toastify";
 import { appContext } from "./context/appContext";
+import LoadingScreen from "./components/LoadingScreen";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const { showSignInModal, handleCloseSignInModal } = useContext(appContext);
+  const [initialLoading, setInitialLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setInitialLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   const toggleDarkMode = () => {
     setDarkMode((prev) => !prev);
@@ -42,64 +52,70 @@ const App = () => {
   return (
     <Router>
       <div className={`${darkMode && "dark"} font-Quicksand min-h-screen`}>
-        <Header
-          toggleDarkMode={toggleDarkMode}
-          toggleSidebar={toggleSidebar}
-          darkMode={darkMode}
-        />
-        <Sidebar isSidebarOpen={isSidebarOpen} />
-        <Main>
-          <ToastContainer />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <DarkModeWrapper>
-                  <Content className="">
-                    <div className="p-4 gap-4  flex flex-col">
-                      <Stats darkMode={darkMode} />
-                      <div className="flex flex-col gap-3 lg:flex-row">
-                        <Team />
-                        <Event />
-                      </div>
-                    </div>
-                  </Content>
-                </DarkModeWrapper>
-              }
+        {initialLoading ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            <Header
+              toggleDarkMode={toggleDarkMode}
+              toggleSidebar={toggleSidebar}
+              darkMode={darkMode}
             />
+            <Sidebar isSidebarOpen={isSidebarOpen} />
+            <Main>
+              <ToastContainer />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <DarkModeWrapper>
+                      <Content className="">
+                        <div className="p-4 gap-4  flex flex-col">
+                          <Stats darkMode={darkMode} />
+                          <div className="flex flex-col gap-3 lg:flex-row">
+                            <Team />
+                            <Event />
+                          </div>
+                        </div>
+                      </Content>
+                    </DarkModeWrapper>
+                  }
+                />
 
-            <Route
-              path="/products"
-              element={
-                <DarkModeWrapper>
-                  <div className="w-full p-4">
-                    <AddProduct />
-                  </div>
-                </DarkModeWrapper>
-              }
-            />
-            <Route
-              path="/foods"
-              element={
-                <DarkModeWrapper>
-                  <div className="w-full p-4">
-                    <FoodList />
-                  </div>
-                </DarkModeWrapper>
-              }
-            />
-            <Route
-              path="/orders"
-              element={
-                <DarkModeWrapper>
-                  <Orders />
-                </DarkModeWrapper>
-              }
-            />
-          </Routes>
-        </Main>
+                <Route
+                  path="/products"
+                  element={
+                    <DarkModeWrapper>
+                      <div className="w-full p-4">
+                        <AddProduct />
+                      </div>
+                    </DarkModeWrapper>
+                  }
+                />
+                <Route
+                  path="/foods"
+                  element={
+                    <DarkModeWrapper>
+                      <div className="w-full p-4">
+                        <FoodList />
+                      </div>
+                    </DarkModeWrapper>
+                  }
+                />
+                <Route
+                  path="/orders"
+                  element={
+                    <DarkModeWrapper>
+                      <Orders />
+                    </DarkModeWrapper>
+                  }
+                />
+              </Routes>
+            </Main>
+          </>
+        )}
       </div>
-      {showSignInModal && <SignInModal onClose={handleCloseSignInModal} />}
+      {!initialLoading && showSignInModal && <SignInModal onClose={handleCloseSignInModal} />}
     </Router>
   );
 };
